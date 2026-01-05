5fecbfc73dcf816b67ce10664804432bd34c992d
Noisycall <github@noisycall.com>
2026-01-05 20:02:49 +0530

    Add file tree resolution

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -1,10 +1,47 @@
-use std::path::PathBuf;
-use std::fs::read_dir;
+use std::path::{Path, PathBuf};
+use std::fs::{read_dir, DirEntry};
 use tauri::{AppHandle, Manager};
 use tauri::path::BaseDirectory;
+use serde::{Deserialize, Serialize};
+
+#[derive(Default,Serialize,Deserialize)]
+pub struct FileTree {
+    is_dir:bool,
+    path_from_docs:String,
+    files:Vec<FileTree>,
+    name:String
+}
+
+fn build_file_tree(docs_path:&String)->FileTree{
+    recursive_file_tree_gen(docs_path,0)
+}
+
+fn recursive_file_tree_gen(path:&String,depth:u8)->FileTree{
+    let mut file_tree = FileTree::default();
+    let file_path = Path::new(path);
+    if !file_path.is_dir() {
+        file_tree.files = vec![];
+        file_tree.name = file_path.file_name().unwrap().to_str().unwrap().to_string();
+        file_tree.path_from_docs = path.to_string();
+        file_tree.is_dir=false;
+        return file_tree;
+    }
+    let val = read_dir(file_path).unwrap();
+    let mut val_sorted = val.map(|x1| {x1.unwrap()}).collect::<Vec<DirEntry>>();
+    val_sorted.sort_by_key(|x2| {x2.path()});
+    let val_collected = val_sorted.iter().map(|x| {
+        let tmp = depth+1;
+        return recursive_file_tree_gen(&x.path().to_str().unwrap().to_string(),tmp);
+    }).collect::<Vec<_>>();
+    file_tree.path_from_docs = path.to_string();
+    file_tree.files = val_collected;
+    file_tree.is_dir = true;
+    file_tree.name =  file_path.file_name().unwrap().to_str().unwrap().to_string();
+    file_tree
+}
 
 #[tauri::command]
-pub fn list_files(app:AppHandle)->(){
+pub fn list_files(app:AppHandle)->FileTree{
     let dir_path_result = get_docs_path(app);
     let dir_path:String;
     match dir_path_result {
@@ -16,9 +53,7 @@ pub fn list_files(app:AppHandle)->(){
             panic!("{}",e)
         }
     }
-    let val = read_dir(dir_path).unwrap();
-    println!("val is {:?}",val.collect::<Vec<_>>());
-    return;
+    build_file_tree(&dir_path)
 }
 
 fn get_docs_path(app: AppHandle) -> tauri::Result<PathBuf> {
@@ -27,8 +62,8 @@ fn get_docs_path(app: AppHandle) -> tauri::Result<PathBuf> {
 
 #[tauri::command]
 pub fn create_new_file(app:AppHandle)->(){
-    let docs_path = get_docs_path(app);
-    let dir = read_dir(docs_path.unwrap()).unwrap();
+    let docs_path = get_docs_path(app).unwrap();
+    let dir = read_dir(&docs_path).unwrap();
     let mut collected_dir = dir.filter_map(|x| {
         let val = x.unwrap().file_name().to_str().unwrap().to_string();
         if val.contains("Untitled"){
@@ -38,5 +73,11 @@ pub fn create_new_file(app:AppHandle)->(){
     }).collect::<Vec<_>>();
     collected_dir.sort();
     println!("collected_dir {:?}",collected_dir);
-    return;
+    if collected_dir.len() <= 0 {
+        let mut new_file_path = (&docs_path.clone()).to_str().unwrap().to_owned();
+        new_file_path.push_str("/Untitled.md");
+        std::fs::File::create(new_file_path).expect("TODO: panic message");
+    }
+    // let new_file_name = Regex::new(r"\d").unwrap().find(collected_dir.iter().rev().next().unwrap()).unwrap().as_str();
+    // println!("{}",new_file_name)
 }
--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -1,30 +1,65 @@
 import { useEffect, useState } from "react";
 import { store } from "@/utils.tsx";
+import { invoke } from "@tauri-apps/api/core";
+
+function FileTreeVis(
+  props: { files: any; root?: boolean } = { files: {}, root: false },
+) {
+  let file = props.files;
+  console.log(file);
+  if (!file.is_dir) {
+    return (
+      <button
+        key={file.path_from_docs}
+        style={{ width: "100%" }}
+        id={file.path_from_docs}
+        onClick={(evt) => {
+          store.selectedFile = evt.currentTarget.id;
+          console.log(store);
+        }}
+      >
+        {file.name}
+      </button>
+    );
+  } else {
+    return (
+      <div className="relative left-2">
+        {props.root ? "" : <span className="relative left-2">{file.name}</span>}
+        {file.files.map((filer: any) => {
+          return <FileTreeVis files={filer} />;
+        })}
+      </div>
+    );
+  }
+}
+
 export default function Sidebar() {
-  let [files, setFiles] = useState([]);
+  let [files, setFiles] = useState({ files: [] } as any);
   useEffect(() => {
     (async () => {
-      invoke("list_files");
-      invoke("create_new_file");
+      let val = await invoke("list_files");
+      // @ts-ignore
+      setFiles(val);
+      console.log(val);
     })();
   }, []);
   return (
     <div id="sidebar" style={{ width: "10em" }}>
-      {files.map((file) => {
-        return (
-          <button
-            style={{ width: "100%" }}
-            id={file}
-            onClick={(evt) => {
-              store.selectedFile = evt.currentTarget.id;
-              console.log(store);
-            }}
-          >
-            {file}
-          </button>
-        );
-      })}
-      <button className="">+</button>
+      <FileTreeVis files={files} root={true} />
+      <button
+        style={{
+          width: "100%",
+          fontWeight: "bolder",
+          fontSize: "2em",
+          border: "solid 1px black",
+        }}
+        onClick={async () => {
+          await invoke("create_new_file");
+          setFiles(await invoke("list_files"));
+        }}
+      >
+        +
+      </button>
     </div>
   );
 }
